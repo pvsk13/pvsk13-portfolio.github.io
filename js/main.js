d0fa74e8f65cbf6770d58a4e4c3962361b98673d
PVSK <pvsk1303@gmail.com>
2024-05-17 21:40:14 -0500

    done

--- js/main.js
+++ js/main.js
@@ -101,17 +101,32 @@
 		});
 	}
 
-	/*--/ Testimonials owl /--*/
-	$('#testimonial-mf').owlCarousel({
-		margin: 20,
-		autoplay: true,
-		autoplayTimeout: 4000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1,
-			}
+	new Swiper('.testimonials-slider', {
+		speed: 600,
+		loop: true,
+		autoplay: {
+		  delay: 5000,
+		  disableOnInteraction: false
+		},
+		slidesPerView: 'auto',
+		pagination: {
+		  el: '.swiper-pagination',
+		  type: 'bullets',
+		  clickable: true
 		}
-	});
+	  });
+	
+	/*--/ Testimonials owl /--*/
+	// $('#testimonial-mf').owlCarousel({
+	// 	margin: 20,
+	// 	autoplay: true,
+	// 	autoplayTimeout: 4000,
+	// 	autoplayHoverPause: true,
+	// 	responsive: {
+	// 		0: {
+	// 			items: 2,
+	// 		}
+	// 	}
+	// });
 
 })(jQuery);
